804f1ec6103c944eee35d5b70e3f00d4dcba869c
nspacer <89945413+nspacer@users.noreply.github.com>
2023-02-18 20:10:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ AwsCdkAppDemoStack(app, "AwsCdkAppDemoStack",
                    # Uncomment the next line if you know exactly what Account and Region you
                    # want to deploy the stack to. */
 
-                   env=cdk.Environment(account='798616169137', region='us-east-1'),
+                   env=cdk.Environment(account='372775801647', region='eu-central-1'),
 
                    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
                    )
